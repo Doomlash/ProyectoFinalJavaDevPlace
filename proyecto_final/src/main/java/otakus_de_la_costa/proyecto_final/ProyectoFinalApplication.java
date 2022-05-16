19c6394625ab6fb495c8093a08f4910a45029530
Doomlash <emivilche123@gmail.com>
2022-05-16 19:14:31 -0300

    init

---
+++ proyecto_final/src/main/java/otakus_de_la_costa/proyecto_final/ProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package otakus_de_la_costa.proyecto_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ proyecto_final/src/test/java/otakus_de_la_costa/proyecto_final/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package otakus_de_la_costa.proyecto_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
