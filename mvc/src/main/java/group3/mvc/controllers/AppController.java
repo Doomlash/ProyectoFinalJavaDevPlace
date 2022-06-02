41deb7a68ace7b8305b7974c54987d5cf98ae2ac
Doomlash <emivilche123@gmail.com>
2022-06-01 21:59:21 -0300

    /mvc for AppController

--- mvc/src/main/java/group3/mvc/controllers/AppController.java
+++ mvc/src/main/java/group3/mvc/controllers/AppController.java
@@ -1,23 +1,28 @@
 package group3.mvc.controllers;
 
-import group3.mvc.model.MyUser;
-import group3.mvc.services.MyUserService;
-import group3.mvc.services.implementation.IGroup;
-import group3.mvc.services.implementation.IMessages;
-import group3.mvc.services.implementation.IMyUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import group3.mvc.model.FormRequest;
+import group3.mvc.model.MyUser;
 import group3.mvc.model.UserHolder;
 import group3.mvc.services.MiddleService;
+import group3.mvc.services.implementation.IGroup;
+import group3.mvc.services.implementation.IMessages;
+import group3.mvc.services.implementation.IMyUser;
 
 
 @Controller
+@RequestMapping("/mvc")
 public class AppController {
 
     @Autowired
@@ -40,13 +45,13 @@ public class AppController {
     @GetMapping("/loginSuccess")
     public String initLogin(){
         s.login();
-        return "redirect:/home";
+        return "redirect:/mvc/chatRoom";
     }
 
     @GetMapping("/registerSuccess")
     public String initRegister(){
         s.register();
-        return "redirect:/home";
+        return "redirect:/mvc/chatRoom";
     }
 
     @GetMapping("/home")
@@ -85,7 +90,7 @@ public class AppController {
     public String addContact(@ModelAttribute("users") MyUser user, Model model){
         UserHolder.setCurrentUser(iMU.readUByUsername("pataPollo"));
         Integer rta = iMU.addC(user);
-        return "redirect:/addContact";
+        return "redirect:/mvc/addContact";
     }
 
     ////////ADDGROUP/////
@@ -97,13 +102,13 @@ public class AppController {
     @GetMapping("/searchG")
     public String searchG(@RequestParam("username") String username, Model model){
         model.addAttribute("users",iMU.readUByUsername(username));
-        return "redirect:/addGroup";
+        return "redirect:/mvc/addGroup";
     }
 
     @PostMapping("/{idG}/group/{idC}")//////("{idGroup}/addGroup/{idC}")
     public String addGroup(@PathVariable("idG") Long idG, @PathVariable("idC") Long idC){
         Integer rta = iG.addM(idG,idC);
-        return "redirect:/addGroup";
+        return "redirect:/mvc/addGroup";
     }
 
 
@@ -116,20 +121,20 @@ public class AppController {
     @PostMapping("/contact/removeC/{idC}")
     public String removeContact(@PathVariable("idC")Long idC){
         Integer rta = iMU.removeC(idC);
-        return "redirect:/chatRoom";
+        return "redirect:/mvc/chatRoom";
     }
 
     @PostMapping("/contact/addB/{idC}")
     public String addBlock(@ModelAttribute("block") MyUser block, Model model){
         Integer rta = iMU.addB(block);
-        return "redirect:/chatRoom";
+        return "redirect:/mvc/chatRoom";
     }
 
 
     @PostMapping("/group/delete/{idG}")
     public String delGroup(@PathVariable("idG")Long idG){
         Integer rta = iG.deleteG(idG);
-        return "redirect:/chatRoom";
+        return "redirect:/mvc/chatRoom";
     }
 
 
